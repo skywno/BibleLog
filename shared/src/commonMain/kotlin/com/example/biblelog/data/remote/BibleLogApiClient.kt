0fba715efbb154cb9ee370f87cd1b44ddca9754d
Gyeha Lim <skywno@gmail.com>
2026-07-11 16:25:54 +0300

    Add import

--- shared/src/commonMain/kotlin/com/example/biblelog/data/remote/BibleLogApiClient.kt
+++ shared/src/commonMain/kotlin/com/example/biblelog/data/remote/BibleLogApiClient.kt
@@ -2,6 +2,7 @@ package com.example.biblelog.data.remote
 
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.websocket.WebSockets
 import io.ktor.client.plugins.defaultRequest
 import io.ktor.client.plugins.logging.LogLevel
